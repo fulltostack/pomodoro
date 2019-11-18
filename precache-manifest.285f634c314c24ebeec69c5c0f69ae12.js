4cabf8a7ad99e8303c7f6984c0e75f40eae94520
fulltostack <fulltostack@gmail.com>
2019-11-18 14:11:40 +0530

    Updates

---
+++ precache-manifest.285f634c314c24ebeec69c5c0f69ae12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a25c8f67b9dbe92b7868279b05d463",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "8c1cdb45a21ee4061c3a",
+    "url": "/pomodoro/static/css/main.d0b4d37b.chunk.css"
+  },
+  {
+    "revision": "fce3309af17a08030aa1",
+    "url": "/pomodoro/static/js/2.9271b168.chunk.js"
+  },
+  {
+    "revision": "8c1cdb45a21ee4061c3a",
+    "url": "/pomodoro/static/js/main.9677a44b.chunk.js"
+  },
+  {
+    "revision": "1660f1d96f94069b7c8b",
+    "url": "/pomodoro/static/js/runtime-main.b6fa8493.js"
+  }
+]);
